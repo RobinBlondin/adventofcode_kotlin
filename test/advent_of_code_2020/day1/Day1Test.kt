611ed7825237ead05b02900610428479246e15ee
Robin Blondin <robin.blondin@protonmail.com>
2024-02-23 10:35:21 +0100

    Created Tests for day1

---
+++ test/advent_of_code_2020/day1/Day1Test.kt
@@ -0,0 +1,19 @@
+package advent_of_code_2020.day1
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+
+class Day1Test {
+
+    @Test
+    fun testSolution(): Unit {
+        val day1 = Day1()
+        val path = "test/advent_of_code_2020/day1/input.txt"
+        val inputList = day1.readFileToList(path)
+
+
+        assertEquals(514579, day1.solutionA(inputList))
+        assertEquals(241861950, day1.solutionB(inputList))
+    }
+}
