7b3817a83f550737e52448531581849601966c1f
Robin Blondin <robin.blondin@protonmail.com>
2024-02-27 12:27:27 +0100

    Added other solution for day5

--- sheet.ws.kts
+++ sheet.ws.kts
@@ -1,2 +1,7 @@
-val value = "#ab12d3"
-value matches "#[0-9,a-f]{6}".toRegex()
+val value = "BBFFBBFRLL"
+val str = value.replace("B", "1")
+    .replace("R", "1")
+    .replace("F", "0")
+    .replace("L", "0").toByte().toInt(2)
+
+//
