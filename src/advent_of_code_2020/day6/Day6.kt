a1239a3bbafd8b8a2bfb4dcada9d9aee6d83b811
Robin Blondin <robin.blondin@protonmail.com>
2024-02-27 20:51:38 +0100

    removed code for another attempt

--- src/advent_of_code_2020/day6/Day6.kt
+++ src/advent_of_code_2020/day6/Day6.kt
@@ -4,47 +4,41 @@ import java.io.File
 
 class Day6(val path: String = "src/advent_of_code_2020/day6/input.txt") {
 
-    val listOfYes = File(path).readText()
+    val listOfUniqueYeses = File(path).readText()
         .trim()
         .split("\n\n")
         .map {
             it.split(" ", "\n")
                 .joinToString { a -> a }
                 .replace(", ", "")
-                .toCharArray()
+                .toCharArray().map { b -> b.toString() }
                 .toSet().toList()
         }
         .toList()
 
-    val list = File(path).readText()
+    val listOfStrings = File(path).readText()
         .trim()
         .split("\n\n")
         .map { it.split(" ", "\n") }
         .toList()
 
 
-    fun solutionA(): Int = listOfYes.sumOf { it.size }
+    fun solutionA(): Int = listOfUniqueYeses.sumOf { it.size }
     fun solutionB(): Int {
         var count = 0
-        for (i in list.indices) {
-            for (j in list[i].indices) {
-                var count2 = 0
-                for (k in listOfYes.indices) {
-                    for (l in listOfYes[k].indices) {
-                        if (list[i][j].contains(listOfYes[k][l])) count2++
-                    }
-                }
-                println(count2)
-                if (count2 == list[i].size) count++
+        listOfStrings.forEach { list ->
+            list.forEach { element ->
+               element.toCharArray().forEach { a ->
+                   if (element.contains(a)) count++
+               }
             }
         }
-        return count
     }
 }
 
 fun main() {
     val day6 = Day6()
 
-
+    println(day6.listOfStrings.forEach{ println(it)})
     println(day6.solutionB())
 }
