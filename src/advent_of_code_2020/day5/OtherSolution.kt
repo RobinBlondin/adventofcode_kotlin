76096baa9eb6cbc922dd974feae732bb8e8920a2
Robin Blondin <robin.blondin@protonmail.com>
2024-02-27 09:02:21 +0100

    working on another solution to day 5

---
+++ src/advent_of_code_2020/day5/OtherSolution.kt
@@ -0,0 +1,8 @@
+package advent_of_code_2020.day5
+/*
+    Jag började titta på en lösning på youtube och de nämnde direkt likheten mellan input-strängarna och binärkod.
+    Så jag gick ut på nätet och matade in en rad i en konverterare där jag bytte ut bokstäverna mot ettor och nollor och voila!
+    Så jag testar att göra en ny lösning baserat på det.
+ */
+class OtherSolution {
+}
