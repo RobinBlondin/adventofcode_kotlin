4aabbc5f4d86b12f3f46c9510a597c10410fb15c
Robin Blondin <robin.blondin@protonmail.com>
2024-02-27 12:27:13 +0100

    Added tests and started solving day6

---
+++ test/advent_of_code_2020/day6/Day6Test.kt
@@ -0,0 +1,15 @@
+package advent_of_code_2020.day6
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day6Test {
+
+    @Test
+    fun solutionA() {
+        val day6 = Day6("test/advent_of_code_2020/day6/input.txt")
+
+        assertEquals(11, day6.solutionA())
+    }
+}
