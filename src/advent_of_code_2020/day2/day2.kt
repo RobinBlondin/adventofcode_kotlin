d35060c875a5ec7cf4bcac3b2dd3f041d4c55845
Robin Blondin <robin.blondin@protonmail.com>
2024-03-04 13:47:01 +0100

    improved solutions

--- src/advent_of_code_2020/day2/day2.kt
+++ src/advent_of_code_2020/day2/day2.kt
@@ -2,23 +2,23 @@ package advent_of_code_2020.day2
 
 import java.io.File
 
+class Day2(path: String = "src/advent_of_code_2020/day2/input.txt") {
+    val list = File(path).readLines()
+    fun solutionA(): Int = list.count {
+        val (first, last, letter, password) = it.split(": ", " ", "-")
+        password.count { char -> char.toString() == letter } in first.toInt()..last.toInt()
+    }
+
+    fun solutionB(): Int = list.count {
+        val (first, last, letter, password) = it.split(": ", " ", "-")
+        (password[first.toInt() - 1].toString() == letter && password[last.toInt() - 1].toString() != letter) ||
+                (password[last.toInt() - 1].toString() == letter && password[first.toInt() - 1].toString() != letter)
+    }
+}
+
 fun main() {
     val day2 = Day2()
 
     println("Solution A: ${day2.solutionA()}")
     println("Solution B: ${day2.solutionB()}")
-}
-
-class Day2(path: String = "src/advent_of_code_2020/day2/input.txt") {
-    val list = File(path).readLines()
-    fun solutionA(): Int = list.count {
-        val arr = it.split(": ", " ", "-")
-        (arr[3].count { a -> a.toString() == arr[2] } in IntRange(arr[0].toInt(), arr[1].toInt()))
-    }
-
-    fun solutionB(): Int = list.count {
-        val arr = it.split(": ", " ", "-")
-        (arr[3][arr[0].toInt() - 1].toString() == arr[2] && arr[3][arr[1].toInt() - 1].toString() != arr[2]) ||
-                (arr[3][arr[1].toInt() - 1].toString() == arr[2] && arr[3][arr[0].toInt() - 1].toString() != arr[2])
-    }
 }
