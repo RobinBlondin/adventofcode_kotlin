44f1af78bd1d700b355fb090f105ed2b4013cb53
Robin Blondin <robin.blondin@protonmail.com>
2024-03-05 13:21:48 +0100

    removed redundant code

--- src/advent_of_code_2020/day8/Day8.kt
+++ src/advent_of_code_2020/day8/Day8.kt
@@ -3,55 +3,49 @@ package advent_of_code_2020.day8
 import java.io.File
 
 typealias Pairs = Pair<String, Int>
+typealias Sum = Int
+typealias BootSuccessful = Boolean
 
 class Day8(path: String = "src/advent_of_code_2020/day8/input.txt") {
-    val list: List<Pairs> = File(path).readLines()
+    private val instructions: List<Pairs> = File(path).readLines()
         .map { line ->
             val (operation, value) = line.split(" ")
             Pair(operation, value.toInt())
         }
 
-    private val usedOperations = mutableSetOf<Pair<Pairs, Int>>()
+    private val executedInstructions = mutableSetOf<Int>()
 
-    private fun countAccumulator(index: Int = 0, sum: Int = 0, list: List<Pairs> = this.list): Pair<Int, Boolean> {
+    private fun countAccumulator(index: Int = 0, sum: Int = 0, list: List<Pairs> = instructions): Pair<Sum, BootSuccessful> {
         if (index + 1 > list.size) return Pair(sum, true)
-        val currentPair = list[index]
-        val currentCommand = list[index].first
+        val currentOperation = list[index].first
         val value = list[index].second
 
-        if (usedOperations.contains(Pair(currentPair, index))) return Pair(sum, false)
+        if (executedInstructions.contains(index))
+            return Pair(sum, false)
+        else
+            executedInstructions.add(index)
 
-        usedOperations.add(Pair(currentPair, index))
-
-        return when (currentCommand) {
+        return when (currentOperation) {
             "acc" -> countAccumulator(index + 1, sum + value, list)
             "jmp" -> countAccumulator(index + value, sum, list)
             else -> countAccumulator(index + 1, sum, list)
         }
     }
 
-    private fun testMutatedList(list: MutableList<Pairs>): Int {
-        usedOperations.clear()
-        try {
-            val temp = countAccumulator(list = list)
-            return if (temp.second) temp.first else -1
-        } catch (e: Exception) {
-            return -1
-        }
-    }
-
     fun solutionA(): Int = countAccumulator().first
     fun solutionB(): Int {
-        val mutatedList = list.toMutableList()
+        val mutatedList = instructions.toMutableList()
 
         for (i in mutatedList.indices) {
-            val before = mutatedList[i]
-            when (before.first) {
-                "jmp" -> mutatedList[i] = Pair("nop", before.second)
-                "nop" -> if (before.second != 0) mutatedList[i] = Pair("jmp", before.second)
+            val currentInstruction = mutatedList[i]
+            when (currentInstruction.first) {
+                "jmp" -> mutatedList[i] = Pair("nop", currentInstruction.second)
+                "nop" -> if (currentInstruction.second != 0) mutatedList[i] = Pair("jmp", currentInstruction.second)
+                else -> continue
             }
-            val temp = testMutatedList(mutatedList)
-            if (temp > 0) return temp else mutatedList[i] = before
+            executedInstructions.clear()
+            val temp = countAccumulator(list = mutatedList)
+            if (temp.second) return temp.first else mutatedList[i] = currentInstruction
         }
         return -1
     }
