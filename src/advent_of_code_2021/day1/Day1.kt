da343d02ae6a2d9759c8fbaad8726bbcc3e61319
Robin Blondin <robin.blondin@protonmail.com>
2024-04-21 19:19:55 +0200

    Added solution for day1 2021

---
+++ src/advent_of_code_2021/day1/Day1.kt
@@ -0,0 +1,27 @@
+package advent_of_code_2021.day1
+
+import advent_of_code_2021.day3.Day3
+import java.io.File
+
+class Day1(path: String = "./src/advent_of_code_2021/day1/input.txt")  {
+    val list = File(path).readLines().map { it.toInt() }
+
+    fun solutionA(input: List<Int>): Int {
+        val sublist = input.subList(1, input.size);
+        return sublist.filterIndexed { index, i ->
+            i > input[index]
+         }.toList().size
+    }
+
+    fun solutionB(input: List<Int>): Int {
+        val list = input.windowed(3, 1).map {it.sum()}
+        return solutionA(list);
+    }
+}
+
+fun main() {
+    val day1 = Day1()
+
+    println("SolutionA: ${day1.solutionA(day1.list)}")
+    println("SolutionB: ${day1.solutionB(day1.list)}")
+}
