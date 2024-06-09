b0f2a7aee833fde96d0c29ce655d1035f80ec860
Robin Blondin <robin.blondin@protonmail.com>
2024-06-09 21:35:27 +0200

    Finished day 8

--- src/advent_of_code_2021/day8/Day8.kt
+++ src/advent_of_code_2021/day8/Day8.kt
@@ -3,69 +3,69 @@ package advent_of_code_2021.day8
 import java.io.File
 
 class Day8(path: String = "./src/advent_of_code_2021/day8/input.txt") {
-    val input = File(path).readLines()
-    val numberCodes = input.map { it.split(" | ")[0].split(" ") }
-    val numbers = input.map { it.split(" | ")[1].split(" ") }
-    private val codeMap = mapOf(
-        "abcdeg" to "0",
-        "acdfg" to "2",
-        "abcdf" to "3",
-        "bcdef" to "5",
-        "bcdefg" to "6",
-        "abcdef" to "9"
-    )
+    private val input = File(path).readLines()
+    private val numberCodes = input.map { it.split(" | ")[0].split(" ") }
+    private val numbers = input.map { it.split(" | ")[1].split(" ") }.map { it.map { value -> value.toList().sorted().joinToString("") } }
 
-    private val uniqueCodes = mapOf(
-        2 to "1",
-        3 to "7",
-        4 to "4",
-        7 to 8)
 
     fun solutionA(): Int {
         return numbers.sumOf { it.count { code -> code.length in 2..4 || code.length == 7 } }
     }
 
     fun solutionB(): Int {
-        val arrayOfCodes = Array(10) {""}
         var sum = 0
+
         for((index, listOfCodes) in numberCodes.withIndex()) {
-            var result = ""
-            populateArrayWithCodes(arrayOfCodes, listOfCodes)
-            for(number in numbers) {
-                result += arrayOfCodes.indexOf(number[index]).toString()
+            val result = StringBuilder()
+            val arrayOfCodes = populateArrayWithCodes(listOfCodes)
+            for(number in numbers[index]) {
+                result.append(arrayOfCodes.indexOf(number).toString())
             }
-            sum += result.toInt()
+            sum += result.toString().toInt()
         }
         return sum
     }
 
-    fun populateArrayWithUniqueCodes(array: Array<String>, listOfCodes: List<String>) {
+    /**
+     * Populates an array with the codes for the digits with unique amount of segments(1, 4, 7 and 8)
+     */
+    private fun populateArrayWithUniqueCodes(listOfCodes: List<String>): Array<String> {
+        val array = Array(10) { "" }
         for (numberCode in listOfCodes) {
             when(numberCode.length) {
-                2 or 3 or 4 or 7 -> array[1] = numberCode
+                2 -> array[1] = numberCode
+                3 -> array[7] = numberCode
+                4 -> array[4] = numberCode
+                7 -> array[8] = numberCode
             }
         }
+        return array
     }
 
-    fun populateArrayWithCodes(array: Array<String>, listOfCodes: List<String>) {
-        populateArrayWithUniqueCodes(array, listOfCodes)
+    /**
+     * Populates an array with the codes for the digits that not have unique amount of segments. Compares the non-unique
+     * with the unique using the method of exclusion.
+     */
+    private fun populateArrayWithCodes(listOfCodes: List<String>): Array<String> {
+        val array = populateArrayWithUniqueCodes(listOfCodes)
+
         while(!sumIsEqual(array, listOfCodes)) {
             for (numberCode in listOfCodes) {
                 when(numberCode.length) {
                     5 -> {
                         if(array[1].toList().count { numberCode.contains(it)  } == 2) {
                             array[3] = numberCode
-                        } else if(array[4].toList().count { numberCode.contains(it) } == 3 ) {
+                        } else if(array[4].toList().count { numberCode.contains(it) } == 3) {
                             array[5] = numberCode
                         } else {
                             array[2] = numberCode
                         }
                     }
                     6 -> {
-                        if(array[8].toList().count { numberCode.contains(it) } == 6) {
-                            array[0] = numberCode
-                        } else if (array[4].toList().count { numberCode.contains(it) } == 4) {
+                        if(array[4].toList().count { numberCode.contains(it) } == 4) {
                             array[9] = numberCode
+                        } else if (array[4].toList().count { numberCode.contains(it) } == 3 && array[7].toList().count { numberCode.contains(it)} == 3) {
+                            array[0] = numberCode
                         } else {
                             array[6] = numberCode
                         }
@@ -73,9 +73,16 @@ class Day8(path: String = "./src/advent_of_code_2021/day8/input.txt") {
                 }
             }
         }
+       array.forEachIndexed { index, value ->
+           array[index] = value.toList().sorted().joinToString("") }
+
+        return array
     }
 
-    fun sumIsEqual(array: Array<String>, listOfCodes: List<String>): Boolean =
+    /**
+     * returns true if the array is fully populated
+     */
+    private fun sumIsEqual(array: Array<String>, listOfCodes: List<String>): Boolean =
         array.sumOf { it.length } == listOfCodes.sumOf { it.length }
 }
 
@@ -83,5 +90,5 @@ fun main() {
     val day8 = Day8()
 
     println("SolutionA: ${day8.solutionA()}")
-    println("SolutionB: ${day8.solutionB()}}")
+    println("SolutionB: ${day8.solutionB()}")
 }
--- test/advent_of_code_2021/day8/Day8Test.kt
+++ test/advent_of_code_2021/day8/Day8Test.kt
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 class Day8Test {
-
+    val day8 = Day8("./test/advent_of_code_2021/day8/input.txt")
     @Test
     fun solutionA() {
+        assertEquals(26, day8.solutionA())
     }
 
     @Test
     fun solutionB() {
+        assertEquals(61229, day8.solutionB())
     }
 }
