11439a404e62d637c6095ae4b399b9220a393d44
Robin Blondin <robin.blondin@protonmail.com>
2024-06-13 18:39:25 +0200

    Finished day1 2019 part 1

---
+++ test/advent_of_code_2019/day2/Day2Test.kt
@@ -0,0 +1,16 @@
+package advent_of_code_2019.day2
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day2Test {
+
+    @Test
+    fun solutionA() {
+    }
+
+    @Test
+    fun solutionB() {
+    }
+}
