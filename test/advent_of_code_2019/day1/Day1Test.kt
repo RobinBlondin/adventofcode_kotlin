ad51cf599f5dd883e851afa110535106a7f04e30
Robin Blondin <robin.blondin@protonmail.com>
2024-06-13 13:47:59 +0200

    Created tests for day1 2019

---
+++ test/advent_of_code_2019/day1/Day1Test.kt
@@ -0,0 +1,42 @@
+package advent_of_code_2019.day1
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day1Test {
+    val day1 = Day1("./test/advent_of_code_2019/day1/input.txt")
+    @Test
+    fun solutionA() {
+        assertEquals(34241, day1.solutionA())
+    }
+
+    @Test
+    fun solutionB() {
+        assertEquals(51316, day1.solutionB())
+    }
+
+    @Test
+    fun testCalculateFuel() {
+        val input1 = 12L
+        val input2 = 100756L
+
+        val expectedResult1 = 2L
+        val expectedResult2 = 33583L
+
+        assertEquals(expectedResult1, day1.calculateFuel(input1))
+        assertEquals(expectedResult2, day1.calculateFuel(input2))
+    }
+
+    @Test
+    fun testCalculateFuelRecursive() {
+        val input1 = 1969L
+        val input2 = 100756L
+
+        val expectedResult1 = 966L
+        val expectedResult2 = 50346L
+
+        assertEquals(expectedResult1, day1.calculateFuelRecursive(input1, 0))
+        assertEquals(expectedResult2, day1.calculateFuelRecursive(input2, 0))
+    }
+}
