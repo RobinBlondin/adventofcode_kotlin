4102e2957e49d1de18795cf8527b77a2b729ceda
Robin Blondin <robin.blondin@protonmail.com>
2024-12-02 17:22:49 +0100

    Set up files for day3

---
+++ src/advent_of_code_2024/day3/Day3.kt
@@ -0,0 +1,22 @@
+package advent_of_code_2024.day3
+
+import java.io.File
+
+class Day3(path: String = "./src/advent_of_code_2024/day3/input.txt") {
+    val input = File(path).readLines()
+
+    fun solutionA(): Int {
+        return 0
+    }
+
+    fun solutionB(): Int {
+        return 0
+    }
+}
+
+fun main() {
+    val day3 = Day3()
+
+    println("SolutionA: ${day3.solutionA()}")
+    println("SolutionB: ${day3.solutionB()}")
+}
