54ff108c476e8039698cad85f8173c96870cfb1e
Robin Blondin <robin.blondin@protonmail.com>
2024-12-05 20:48:59 +0100

    Updated gitignore to remove advent of codes inputs from version control

---
+++ src/advent_of_code_2023/day10/Day10.kt
@@ -0,0 +1,22 @@
+package advent_of_code_2023.day10
+
+import java.io.File
+
+class Day10(path: String = "./src/advent_of_code_2023/day10/input.txt") {
+    private val input = File(path).readLines()
+
+    fun solutionA(): Int {
+        return 0
+    }
+
+    fun solutionB(): Int {
+        return 0
+    }
+}
+
+fun main() {
+    val day10 = Day10()
+
+    println("SolutionA: ${day10.solutionA()}")
+    println("SolutionB: ${day10.solutionB()}")
+}
