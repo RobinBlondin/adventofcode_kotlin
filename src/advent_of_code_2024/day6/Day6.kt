8756c29fee36fb5132c7c24fe05cb765f6046e11
Robin Blondin <robin.blondin@protonmail.com>
2024-12-07 02:05:17 +0100

    Completed day 6

--- src/advent_of_code_2024/day6/Day6.kt
+++ src/advent_of_code_2024/day6/Day6.kt
@@ -7,45 +7,31 @@ class Day6(path: String = "./src/advent_of_code_2024/day6/input.txt") {
     val input = File(path).readLines().map { it.toCharArray() }
     private val startingPoint = setStartingPoint()
 
-    fun solutionA(): Int = mapPatrolPath()
+    fun solutionA(): Int = mapPatrolPath(input).size
 
-    fun solutionB(): Int {
-        var count = 0
-        input.forEachIndexed { i, row ->
-            row.forEachIndexed { j, cell ->
-                if (cell == '.') {
-                    input[i][j] = '#'
-
-
-                    val result = mapPatrolPath()
-                    if (result == -1) {
-                        count++
-
-                    }
-
-                    input[i][j] = '.'
-                }
+    fun solutionB(): Int =
+            mapPatrolPath(input).count {
+                val copy = input.map { list -> list.copyOf() }
+                copy[it.first][it.second] = '#'
+                mapPatrolPath(copy).isEmpty()
             }
-        }
-        return count
-    }
 
-    private fun mapPatrolPath(): Int {
-
-        val visited = mutableSetOf(startingPoint)
+    private fun mapPatrolPath(matrix: List<CharArray>): Set<Pair<Int, Int>> {
         var currentPoint = startingPoint
         var direction = Direction.NORTH
-        var visitedCounter = 0
+        val visited = mutableSetOf(startingPoint)
+        var count = 0
 
-        while(!isNextMoveOutOfBounds(currentPoint,direction)) {
-
-            if(visitedCounter == 1000000) {
-                return -1
+        while (!isNextMoveOutOfBounds(currentPoint, direction, matrix)) {
+            if(count == 1000) {
+                return setOf()
             }
 
-            direction = if(isNextMoveObstruction(currentPoint, direction)) nextDirection(direction) else direction
+            while (isNextMoveObstruction(currentPoint, direction, matrix)) {
+                direction = nextDirection(direction)
+            }
 
-            currentPoint = when(direction) {
+            currentPoint = when (direction) {
                 Direction.NORTH -> Pair(currentPoint.first - 1, currentPoint.second)
                 Direction.SOUTH -> Pair(currentPoint.first + 1, currentPoint.second)
                 Direction.EAST -> Pair(currentPoint.first, currentPoint.second + 1)
@@ -53,40 +39,38 @@ class Day6(path: String = "./src/advent_of_code_2024/day6/input.txt") {
             }
 
             if(visited.contains(currentPoint)) {
-                visitedCounter++
+                count++
             } else {
-                visitedCounter = 0
+                count = 0
                 visited.add(currentPoint)
             }
-
         }
-
-        return visited.size
+        return visited
     }
 
-    private fun isOutOfBounds(point: Pair<Int, Int>): Boolean {
-        return point.first < 0 || point.first > input.size - 1 || point.second < 0 || point.second > input.first().size - 1
+    private fun isOutOfBounds(point: Pair<Int, Int>, matrix: List<CharArray>): Boolean {
+        return point.first < 0 || point.first > matrix.size - 1 || point.second < 0 || point.second > matrix.first().size - 1
     }
 
-    private fun isNextMoveOutOfBounds(point: Pair<Int, Int>, direction: Direction): Boolean {
+    private fun isNextMoveOutOfBounds(point: Pair<Int, Int>, direction: Direction, matrix: List<CharArray>): Boolean {
         return when (direction) {
-            Direction.NORTH -> isOutOfBounds(Pair(point.first - 1, point.second))
-            Direction.SOUTH -> isOutOfBounds(Pair(point.first + 1, point.second))
-            Direction.EAST -> isOutOfBounds(Pair(point.first, point.second + 1))
-            else -> isOutOfBounds(Pair(point.first, point.second - 1))
+            Direction.NORTH -> isOutOfBounds(Pair(point.first - 1, point.second), matrix)
+            Direction.SOUTH -> isOutOfBounds(Pair(point.first + 1, point.second), matrix)
+            Direction.EAST -> isOutOfBounds(Pair(point.first, point.second + 1), matrix)
+            else -> isOutOfBounds(Pair(point.first, point.second - 1), matrix)
         }
     }
 
-    private fun isNextMoveObstruction(point: Pair<Int, Int>, direction: Direction): Boolean {
+    private fun isNextMoveObstruction(point: Pair<Int, Int>, direction: Direction, matrix: List<CharArray>): Boolean {
         val y = point.first
         val x = point.second
 
         val nextMove = when (direction) {
-            Direction.NORTH -> input[y - 1][x]
-            Direction.SOUTH -> input[y + 1][x]
-            Direction.EAST -> input[y][x + 1]
-            Direction.WEST -> input[y][x - 1]
-            else -> input[y][x]
+            Direction.NORTH -> matrix[y - 1][x]
+            Direction.SOUTH -> matrix[y + 1][x]
+            Direction.EAST -> matrix[y][x + 1]
+            Direction.WEST -> matrix[y][x - 1]
+            else -> matrix[y][x]
         }
 
         return nextMove == '#'
